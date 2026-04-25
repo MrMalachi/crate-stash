8a2f4570390948c83762dd8f4adff940981680ed
MrMalachi <brownmnb11@yahoo.com>
2026-04-25 17:09:19 -0400

    Initial commit

---
+++ crate_manager.py
@@ -0,0 +1,27 @@
+"""
+This file contains the main logic of the program - Crate Stash©
+Crate Stash©, is a program for DJs that want to stash tracks that they hear & love
+on a whim, and then later resort to that crate of stashed tracks to download
+to their DJ playlist(s) for later use so they do not forget
+"""
+
+
+class CrateManager:
+    """Manage a crate of tracks for later download and DJ use."""
+
+    def __init__(self):
+        """Initialize variables."""
+        ...  # Represents a placeholder.
+
+    def display_music_genre_selection_menu(self):
+        """Display a neatly formatted menu for the user."""
+        ...
+
+    def select_music_genre(self):
+        """
+        Prompt user to select a genre of music from the given list & return.
+        """
+        ...
+
+    def add_track_to_crate(self):
+        """Prompt user to enter a track to a crate."""
